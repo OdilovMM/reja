cc1b11b097f0ba5126687b65cfd473eb10aba6e8
OdilovMM <dssrinman@gmail.com>
2023-09-26 03:35:16 +0900

    feat: [08] develop author user and front end web page development

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-      console.log("ERROR:", err);
-    } else {
-      user = JSON.parse(data);
-    }
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
 
-console.log('============----------===========');
+console.log("============----------===========");
 
 // 1 Kirish code lar
 app.use(express.static("public"));
@@ -22,31 +22,25 @@ app.use(express.urlencoded({ extended: true }));
 
 // 2: session code
 
-console.log('============----------===========');
+console.log("============----------===========");
 
 // 3 views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.post('/create-item', (req, res) => {
-    console.log(req.body);
-    res.json({test: 'success' });
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get('/author', (req, res)  => {
-  res.render('author', {user: user});
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
-
-
-
-app.get('/', function(req, res) {
-    res.render('harid');
+app.get("/", function (req, res) {
+  res.render("harid");
 });
-
-
-
 
 const server = http.createServer(app);
 let PORT = 3000;
